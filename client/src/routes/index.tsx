93ca306b1d956654fdca05d0c7edf5d7744f0077
Kylee Tilley <kyleetilley@gmail.com>
2025-12-01 18:36:28 -0600

    Refactor

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -64,244 +64,245 @@ function RouteComponent() {
 
   const data: ParseResult = query.data!;
 
-  return fileStore.file === null ? (
-    <Stack>
-      <Uploader onUpload={handleDropDrop} />
-    </Stack>
-  ) : typeof data !== "undefined" ? (
+  return (
     <Stack>
       <Uploader onUpload={handleDropDrop} />
 
-      <Tabs defaultValue="details">
-        <Tabs.List>
-          <Tabs.Tab value="details">Details</Tabs.Tab>
-          <Tabs.Tab value="raw">Raw</Tabs.Tab>
-        </Tabs.List>
+      {typeof data !== "undefined" && (
+        <>
+          <Tabs defaultValue="details">
+            <Tabs.List>
+              <Tabs.Tab value="details">Details</Tabs.Tab>
+              <Tabs.Tab value="raw">Raw</Tabs.Tab>
+            </Tabs.List>
 
-        <Tabs.Panel value="details" p="md">
-          <Stack>
-            <Group align="top">
-              <Text>{fileStore.file.fileName}</Text>
-              <ButtonGroup>
-                <CloseRequestFileButton onClick={fileStore.unsetFile} />
-              </ButtonGroup>
-            </Group>
+            <Tabs.Panel value="details" p="md">
+              <Stack>
+                <Group align="top">
+                  <Text>{fileStore.file!.fileName}</Text>
+                  <ButtonGroup>
+                    <CloseRequestFileButton onClick={fileStore.unsetFile} />
+                  </ButtonGroup>
+                </Group>
 
-            <Card>
-              <Title order={3}>Request</Title>
+                <Card>
+                  <Title order={3}>Request</Title>
 
-              <Table>
-                <Table.Thead>
-                  <Table.Tr>
-                    <Table.Th>Verb</Table.Th>
-                    <Table.Th>Target</Table.Th>
-                    <Table.Th>Version</Table.Th>
-                  </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>
-                  <Table.Tr>
-                    <Table.Td>{data.request.verb}</Table.Td>
-                    <Table.Td>{data.request.target}</Table.Td>
-                    <Table.Td>{data.request.http_version}</Table.Td>
-                  </Table.Tr>
-                </Table.Tbody>
-              </Table>
-
-              {data.request.headers.length > 0 && (
-                <>
-                  <Title order={4}>Headers</Title>
                   <Table>
                     <Table.Thead>
                       <Table.Tr>
-                        <Table.Th>Header</Table.Th>
-                        <Table.Th>Value</Table.Th>
+                        <Table.Th>Verb</Table.Th>
+                        <Table.Th>Target</Table.Th>
+                        <Table.Th>Version</Table.Th>
                       </Table.Tr>
                     </Table.Thead>
                     <Table.Tbody>
-                      {data.request.headers.map((header, i) => {
-                        return (
-                          <Table.Tr key={i}>
-                            <Table.Td>{header[0]}</Table.Td>
-                            <Table.Td>{header[1]}</Table.Td>
-                          </Table.Tr>
-                        );
-                      })}
+                      <Table.Tr>
+                        <Table.Td>{data.request.verb}</Table.Td>
+                        <Table.Td>{data.request.target}</Table.Td>
+                        <Table.Td>{data.request.http_version}</Table.Td>
+                      </Table.Tr>
                     </Table.Tbody>
                   </Table>
-                </>
-              )}
 
-              {(data.request.body?.length ?? 0) > 0 && (
-                <>
-                  <Title order={4}>Body</Title>
-                  <Code block>{data.request.body}</Code>
-                </>
-              )}
-
-              {data.full.response && (
-                <>
-                  <Title order={4} onClick={toggleExpectedResponse}>
-                    Expected Response
-                  </Title>
-
-                  <Collapse in={expectedResponseOpened}>
-                    <Card>
+                  {data.request.headers.length > 0 && (
+                    <>
+                      <Title order={4}>Headers</Title>
                       <Table>
                         <Table.Thead>
                           <Table.Tr>
-                            <Table.Th>Version</Table.Th>
-                            <Table.Th>Status Code</Table.Th>
-                            <Table.Th>Status Text</Table.Th>
+                            <Table.Th>Header</Table.Th>
+                            <Table.Th>Value</Table.Th>
                           </Table.Tr>
                         </Table.Thead>
                         <Table.Tbody>
-                          <Table.Tr>
-                            <Table.Td>
-                              {data.full.response[0].http_version}
-                            </Table.Td>
-                            <Table.Td>
-                              {data.full.response[0].status_code}
-                            </Table.Td>
-                            <Table.Td>
-                              {data.full.response[0].status_text}
-                            </Table.Td>
-                          </Table.Tr>
+                          {data.request.headers.map((header, i) => {
+                            return (
+                              <Table.Tr key={i}>
+                                <Table.Td>{header[0]}</Table.Td>
+                                <Table.Td>{header[1]}</Table.Td>
+                              </Table.Tr>
+                            );
+                          })}
                         </Table.Tbody>
                       </Table>
+                    </>
+                  )}
 
-                      {data.full.response[0].headers.length > 0 && (
-                        <>
-                          <Title order={5}>Headers</Title>
+                  {(data.request.body?.length ?? 0) > 0 && (
+                    <>
+                      <Title order={4}>Body</Title>
+                      <Code block>{data.request.body}</Code>
+                    </>
+                  )}
+
+                  {data.full.response && (
+                    <>
+                      <Title order={4} onClick={toggleExpectedResponse}>
+                        Expected Response
+                      </Title>
+
+                      <Collapse in={expectedResponseOpened}>
+                        <Card>
                           <Table>
                             <Table.Thead>
                               <Table.Tr>
-                                <Table.Th>Header</Table.Th>
-                                <Table.Th>Value</Table.Th>
+                                <Table.Th>Version</Table.Th>
+                                <Table.Th>Status Code</Table.Th>
+                                <Table.Th>Status Text</Table.Th>
                               </Table.Tr>
                             </Table.Thead>
                             <Table.Tbody>
-                              {data.full.response[0].headers.map(
-                                (header, i) => {
-                                  return (
-                                    <Table.Tr key={i}>
-                                      <Table.Td>{header[0]}</Table.Td>
-                                      <Table.Td>{header[1]}</Table.Td>
-                                    </Table.Tr>
-                                  );
-                                }
-                              )}
+                              <Table.Tr>
+                                <Table.Td>
+                                  {data.full.response[0].http_version}
+                                </Table.Td>
+                                <Table.Td>
+                                  {data.full.response[0].status_code}
+                                </Table.Td>
+                                <Table.Td>
+                                  {data.full.response[0].status_text}
+                                </Table.Td>
+                              </Table.Tr>
                             </Table.Tbody>
                           </Table>
-                        </>
-                      )}
 
-                      {(data.full.response[0].body?.length ?? 0) > 0 && (
-                        <>
-                          <Title order={5}>Body</Title>
-                          <Code block>{data.full.response[0].body}</Code>
-                        </>
-                      )}
-                    </Card>
-                  </Collapse>
-                </>
-              )}
-            </Card>
+                          {data.full.response[0].headers.length > 0 && (
+                            <>
+                              <Title order={5}>Headers</Title>
+                              <Table>
+                                <Table.Thead>
+                                  <Table.Tr>
+                                    <Table.Th>Header</Table.Th>
+                                    <Table.Th>Value</Table.Th>
+                                  </Table.Tr>
+                                </Table.Thead>
+                                <Table.Tbody>
+                                  {data.full.response[0].headers.map(
+                                    (header, i) => {
+                                      return (
+                                        <Table.Tr key={i}>
+                                          <Table.Td>{header[0]}</Table.Td>
+                                          <Table.Td>{header[1]}</Table.Td>
+                                        </Table.Tr>
+                                      );
+                                    }
+                                  )}
+                                </Table.Tbody>
+                              </Table>
+                            </>
+                          )}
 
-            <Title order={3} onClick={toggleConfiguration}>
-              Configuration
-            </Title>
+                          {(data.full.response[0].body?.length ?? 0) > 0 && (
+                            <>
+                              <Title order={5}>Body</Title>
+                              <Code block>{data.full.response[0].body}</Code>
+                            </>
+                          )}
+                        </Card>
+                      </Collapse>
+                    </>
+                  )}
+                </Card>
 
-            <Collapse in={configurationOpened}>
-              <Title order={4}>Environments</Title>
+                <Title order={3} onClick={toggleConfiguration}>
+                  Configuration
+                </Title>
 
-              {data.envs.length > 0 ? (
-                <Table>
-                  <Table.Tbody>
-                    {data.envs.map((env, i) => {
-                      return (
-                        <Table.Tr key={i}>
-                          <Table.Td>{env}</Table.Td>
+                <Collapse in={configurationOpened}>
+                  <Title order={4}>Environments</Title>
+
+                  {data.envs.length > 0 ? (
+                    <Table>
+                      <Table.Tbody>
+                        {data.envs.map((env, i) => {
+                          return (
+                            <Table.Tr key={i}>
+                              <Table.Td>{env}</Table.Td>
+                            </Table.Tr>
+                          );
+                        })}
+                      </Table.Tbody>
+                    </Table>
+                  ) : (
+                    <Text>No environments defined</Text>
+                  )}
+
+                  <Title order={4}>Parameters</Title>
+
+                  <Title order={5}>Variables</Title>
+
+                  {data.vars.length > 0 ? (
+                    <Table>
+                      <Table.Tbody>
+                        {data.vars.map((v, i) => {
+                          return (
+                            <Table.Tr key={i}>
+                              <Table.Td>{v}</Table.Td>
+                            </Table.Tr>
+                          );
+                        })}
+                      </Table.Tbody>
+                    </Table>
+                  ) : (
+                    <Text>No variables defined</Text>
+                  )}
+
+                  <Title order={5}>Prompts</Title>
+
+                  {data.prompts.length > 0 ? (
+                    <Table>
+                      <Table.Thead>
+                        <Table.Tr>
+                          <Table.Th>Prompt</Table.Th>
+                          <Table.Th>Default Value</Table.Th>
                         </Table.Tr>
-                      );
-                    })}
-                  </Table.Tbody>
-                </Table>
-              ) : (
-                <Text>No environments defined</Text>
-              )}
+                      </Table.Thead>
+                      <Table.Tbody>
+                        {data.prompts.map((prompt, i) => {
+                          return (
+                            <Table.Tr key={i}>
+                              <Table.Td>{prompt}</Table.Td>
+                              <Table.Td>
+                                {data.default_prompt_values[prompt] ??
+                                  "No default"}
+                              </Table.Td>
+                            </Table.Tr>
+                          );
+                        })}
+                      </Table.Tbody>
+                    </Table>
+                  ) : (
+                    <Text>No prompts defined</Text>
+                  )}
 
-              <Title order={4}>Parameters</Title>
+                  <Title order={5}>Secrets</Title>
 
-              <Title order={5}>Variables</Title>
+                  {data.secrets.length > 0 ? (
+                    <Table>
+                      <Table.Tbody>
+                        {data.secrets.map((secret, i) => {
+                          return (
+                            <Table.Tr key={i}>
+                              <Table.Td>{secret}</Table.Td>
+                            </Table.Tr>
+                          );
+                        })}
+                      </Table.Tbody>
+                    </Table>
+                  ) : (
+                    <Text>No secrets defined</Text>
+                  )}
+                </Collapse>
+              </Stack>
+            </Tabs.Panel>
 
-              {data.vars.length > 0 ? (
-                <Table>
-                  <Table.Tbody>
-                    {data.vars.map((v, i) => {
-                      return (
-                        <Table.Tr key={i}>
-                          <Table.Td>{v}</Table.Td>
-                        </Table.Tr>
-                      );
-                    })}
-                  </Table.Tbody>
-                </Table>
-              ) : (
-                <Text>No variables defined</Text>
-              )}
-
-              <Title order={5}>Prompts</Title>
-
-              {data.prompts.length > 0 ? (
-                <Table>
-                  <Table.Thead>
-                    <Table.Tr>
-                      <Table.Th>Prompt</Table.Th>
-                      <Table.Th>Default Value</Table.Th>
-                    </Table.Tr>
-                  </Table.Thead>
-                  <Table.Tbody>
-                    {data.prompts.map((prompt, i) => {
-                      return (
-                        <Table.Tr key={i}>
-                          <Table.Td>{prompt}</Table.Td>
-                          <Table.Td>
-                            {data.default_prompt_values[prompt] ?? "No default"}
-                          </Table.Td>
-                        </Table.Tr>
-                      );
-                    })}
-                  </Table.Tbody>
-                </Table>
-              ) : (
-                <Text>No prompts defined</Text>
-              )}
-
-              <Title order={5}>Secrets</Title>
-
-              {data.secrets.length > 0 ? (
-                <Table>
-                  <Table.Tbody>
-                    {data.secrets.map((secret, i) => {
-                      return (
-                        <Table.Tr key={i}>
-                          <Table.Td>{secret}</Table.Td>
-                        </Table.Tr>
-                      );
-                    })}
-                  </Table.Tbody>
-                </Table>
-              ) : (
-                <Text>No secrets defined</Text>
-              )}
-            </Collapse>
-          </Stack>
-        </Tabs.Panel>
-
-        <Tabs.Panel value="raw" p="md">
-          <Code block>{fileStore.file.text}</Code>
-        </Tabs.Panel>
-      </Tabs>
+            <Tabs.Panel value="raw" p="md">
+              <Code block>{fileStore.file!.text}</Code>
+            </Tabs.Panel>
+          </Tabs>
+        </>
+      )}
     </Stack>
-  ) : null;
+  );
 }
