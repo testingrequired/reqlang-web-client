df41cb78b92dfbadd155734dcc739f0d81e2bd65
Kylee Tilley <kyleetilley@gmail.com>
2025-12-13 19:48:03 -0600

    Add CWD to sidebar in client

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,6 +1,14 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import { AppShell, Burger, Group, NavLink, Title } from "@mantine/core";
+import {
+  AppShell,
+  Badge,
+  Burger,
+  Group,
+  NavLink,
+  Title,
+  Tooltip,
+} from "@mantine/core";
 
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
@@ -11,6 +19,7 @@ import { useServerWebSocket } from "@/hooks/useServerWebSocket";
 import { notifications } from "@mantine/notifications";
 import { useReward } from "react-rewards";
 import { ServerMessage } from "server-types";
+import { useGetDebugInfoQuery } from "@/queries/debug";
 
 export const Route = createRootRoute({
   component: () => {
@@ -50,6 +59,16 @@ export const Route = createRootRoute({
       },
     });
 
+    const debugInfoQuery = useGetDebugInfoQuery();
+
+    if (debugInfoQuery.isPending) {
+      return <p>Loading...</p>;
+    }
+
+    if (debugInfoQuery.isError) {
+      return <p>Error: {debugInfoQuery.error.message}</p>;
+    }
+
     return (
       <AppShell
         header={{ height: 70 }}
@@ -75,6 +94,11 @@ export const Route = createRootRoute({
         </AppShell.Header>
 
         <AppShell.Navbar>
+          <Tooltip label={debugInfoQuery.data.cwd} position="bottom">
+            <Badge size="sm" m="xs" radius="xs" variant="gradient">
+              {debugInfoQuery.data.cwd}
+            </Badge>
+          </Tooltip>
           <NavLink
             label="Request"
             component={Link}
