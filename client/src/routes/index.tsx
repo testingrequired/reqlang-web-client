af9bff9b5503553237378256f5ebfd66c4084d1d
Kylee Tilley <kyleetilley@gmail.com>
2025-12-20 01:51:54 -0600

    Add links to index route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,3 +1,10 @@
+import { useGetDebugInfoQuery } from "@/queries/debug";
+import { Anchor, Card, Group, List, Loader, Text, Title } from "@mantine/core";
+import {
+  IconBrandGithubFilled,
+  IconFile,
+  IconFolderRoot,
+} from "@tabler/icons-react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -5,5 +12,65 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
-  return null;
+  const debugInfoQuery = useGetDebugInfoQuery();
+
+  if (debugInfoQuery.isPending) {
+    return <Loader />;
+  }
+
+  if (debugInfoQuery.isError) {
+    return <p>Error: {debugInfoQuery.error.message}</p>;
+  }
+
+  return (
+    <>
+      <Title order={2}>Project</Title>
+      <Card>
+        <Group>
+          <IconFolderRoot />
+          <Text m={0}>{debugInfoQuery.data.cwd} </Text>
+        </Group>
+      </Card>
+
+      <Title order={2}>Documentation</Title>
+      <Card>
+        <List m={0} listStyleType="none" p={0}>
+          <List.Item>
+            <Group>
+              <IconBrandGithubFilled />
+              <Anchor
+                href="https://github.com/testingrequired/reqlang"
+                target="_blank"
+              >
+                testingrequired/reqlang
+              </Anchor>
+            </Group>
+          </List.Item>
+          <List.Item>
+            <Group>
+              <IconBrandGithubFilled />
+              <Anchor
+                href="https://github.com/testingrequired/reqlang-expr"
+                target="_blank"
+              >
+                testingrequired/reqlang-expr
+              </Anchor>
+            </Group>
+          </List.Item>
+          <List.Item>
+            <Group>
+              <IconFile />
+
+              <Anchor
+                href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/Messages"
+                target="_blank"
+              >
+                HTTP messages - MDN
+              </Anchor>
+            </Group>
+          </List.Item>
+        </List>
+      </Card>
+    </>
+  );
 }
