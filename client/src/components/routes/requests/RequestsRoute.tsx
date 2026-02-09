03fd7b6f988875615b4143e28563f1aaf3576d9b
Kylee Tilley <kyleetilley@gmail.com>
2026-02-08 23:22:34 -0600

    Update wording on "no requests open" message

--- client/src/components/routes/requests/RequestsRoute.tsx
+++ client/src/components/routes/requests/RequestsRoute.tsx
@@ -1,20 +1,16 @@
-import { Alert, Kbd, Stack, Text, ThemeIcon } from "@mantine/core";
+import { Alert, Stack, Text, ThemeIcon } from "@mantine/core";
 import { RequestFilesForm } from "@/components/routes/requests/RequestFilesForm";
 import { OpenRequestFiles } from "@/components/routes/requests/OpenRequestFiles";
 import { useStore } from "zustand";
 import { useRequestFilesStore } from "@/stores/requestFiles";
-import { useOs } from "@mantine/hooks";
 import { IconArrowUp } from "@tabler/icons-react";
 
 export const RequestsRoute = () => {
   const openRequestFilesStore = useStore(useRequestFilesStore);
-  const os = useOs();
 
   const noFilesOpen =
     openRequestFilesStore.openedFiles.length === 0 &&
     openRequestFilesStore.draftFiles.length === 0;
-
-  const modifier = os === "macos" ? "⌘" : "Ctrl";
 
   return (
     <Stack data-testid="requests-view">
@@ -33,8 +29,8 @@ export const RequestsRoute = () => {
             }
           >
             <Text size="sm">
-              Click this button or press <Kbd>{modifier}</Kbd> + <Kbd>O</Kbd> to
-              open files.
+              Click this button open a request or the other button to create a
+              new request.
             </Text>
           </Alert>
         </>
